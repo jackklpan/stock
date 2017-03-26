b178777e4dbabeb0f17f199fb0b3736c70bbfb80
jackklpan <jackklpan@gmail.com>
2017-03-27 00:05:08 +0800

    upload to s3

---
+++ stock_day_upload_to_s3.py
@@ -0,0 +1,10 @@
+import boto3
+import os
+
+client = boto3.client('s3')
+for dir_path, dirs, files in os.walk("./data"):
+    for file_name in files:
+        file_path = os.path.join(dir_path, file_name)
+        if file_path.endswith(".csv"):
+            # print(file_path)
+            client.upload_file(file_path, 'stock-ej04xjp6', file_name)
